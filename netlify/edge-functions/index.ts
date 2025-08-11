241b11829b965b971566af20987ad10331c3d44f
Jake Champion <jake.champion@netlify.com>
2025-08-11 09:34:55 +0100

    Update index.ts

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -1,13 +1,13 @@
-import type { Config } from "@netlify/edge-functions";
+import type { Config } from "@netlify/edge-functions"
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
 import { handle } from 'https://deno.land/x/hono/adapter/netlify/mod.ts'
 
-import { logger } from "https://deno.land/x/hono@v4.3.11/middleware/logger/index.ts";
-import { icon } from './handlers/icon.ts';
-import { item } from './handlers/item.ts';
-import { redirectToTop } from './handlers/redirectToTop.ts';
-import { top } from './handlers/top.ts';
-import { user } from './handlers/user.ts';
+import { logger } from "https://deno.land/x/hono@v4.3.11/middleware/logger/index.ts"
+import { icon } from './handlers/icon.ts'
+import { item } from './handlers/item.ts'
+import { redirectToTop } from './handlers/redirectToTop.ts'
+import { top } from './handlers/top.ts'
+import { user } from './handlers/user.ts'
 
 const app = new Hono()
 
@@ -17,11 +17,11 @@ app.get('/top', redirectToTop)
 app.get('/top/', redirectToTop)
 app.get('/icon.svg', icon)
 app.get('/top/:pageNumber{[1]?[0-9]|20}', (c) => {
-  const pageNumber = Number.parseInt(c.req.param('pageNumber'), 10);
+  const pageNumber = Number.parseInt(c.req.param('pageNumber'), 10)
   return top(pageNumber)
 })
 app.get('/item/:id{[0-9]+}', (c) => {
-  const id = Number.parseInt(c.req.param('id'), 10);
+  const id = Number.parseInt(c.req.param('id'), 10)
   return item(id)
 })
 app.get('/user/:name', (c) => {
