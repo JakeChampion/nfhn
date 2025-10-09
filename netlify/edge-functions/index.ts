60139a6358f0ae64c2a3c2928a76c1b0be449a01
Jake Champion <me@jakechampion.name>
2025-10-09 20:13:52 +0100

    Update index.ts

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -3,7 +3,7 @@ import type { Config } from "@netlify/edge-functions"
 
 export default function() {
   try {
-    return Response.json(type of global)
+    return Response.json(typeof global)
   } catch (er) {
     return new Response(er.message + '\n' + er.stack, {status:500})
   }
