f9916ca710c1fd95ae68be1fbea63ccbf6c3471b
jake champion <jake.champion@netlify.com>
2025-11-17 16:18:04 +0000

    Refactor redirectToTop function into index.ts and remove separate handler file

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -5,9 +5,14 @@ import { handle } from 'https://deno.land/x/hono/adapter/netlify/mod.ts'
 import { logger } from "https://deno.land/x/hono@v4.3.11/middleware/logger/index.ts"
 import { icon } from './handlers/icon.ts'
 import { item } from './handlers/item.ts'
-import { redirectToTop } from './handlers/redirectToTop.ts'
 import { top } from './handlers/top.ts'
 import { user } from './handlers/user.ts'
+
+import { permanentRedirect } from "https://ghuc.cc/worker-tools/response-creators";
+
+export function redirectToTop() {
+  return permanentRedirect("/top/1");
+}
 
 const app = new Hono()
 app.use('*', logger())
@@ -16,6 +21,7 @@ app.get('/top', redirectToTop)
 app.get('/top/', redirectToTop)
 app.get('/icon.svg', icon)
 app.get('/top/:pageNumber{[1]?[0-9]|20}', (c) => {
+
   const pageNumber = Number.parseInt(c.req.param('pageNumber'), 10)
   return top(pageNumber)
 })
