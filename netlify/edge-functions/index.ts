becebd3590ef70d1638e427b48f2025a5ad36b63
Jake Champion <jake.champion@netlify.com>
2025-11-20 09:13:44 +0000

    Update index.ts

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -76,7 +76,7 @@ export default async (request: Request, context: Context) =>{
   console.timeEnd(TIMER_LABEL)
   console.countReset(COUNT_LABEL)
 
-  throw new Error('wow an error was thrown');
+  // throw new Error('wow an error was thrown');
 
   return Response.json({ hello: 'world', location: context.geo.city })
 }
