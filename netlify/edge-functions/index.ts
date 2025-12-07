d6d02cf621cda931b1547969ec5608028713b16b
Jake Champion <me@jakechampion.name>
2025-12-07 09:20:51 +0000

    Update index.ts

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -38,7 +38,30 @@ export const app = new Elysia()
       set.status = 404;
       return 'Not Found';
     }
-    return top(pageNumber, set)
+   
+  const backendResponse = await fetch(
+    `https://api.hnpwa.com/v0/news/${pageNumber}.json`
+  );
+  if (backendResponse.status >= 300) {
+    if (backendResponse.status >= 500) {
+      set.status = 502;
+      return 'Backend service error';
+    }
+    set.status = 404;
+    return 'No such page';
+  }
+  let body = await backendResponse.text()
+  try {
+    let results = JSON.parse(body);
+    if (!results) {
+      set.status = 404;
+      return 'No such page';
+    }
+    set.headers['content-type'] = 'text/html; charset=utf-8';
+    return home(results, pageNumber);
+  } catch (error) {
+    set.status = 500;
+    return `Hacker News API did not return valid JSON.\n\nResponse Body: ${JSON.stringify(body)}`;
   })
   .get('/item/:id', ({ params, set }) => {
     const id = Number.parseInt(params.id, 10)
