63bf7f0dc11f10458a9fe8b3481b4dbf8b84cb7d
Jake Champion <me@jakechampion.name>
2025-12-07 09:56:27 +0000

    Update index.ts

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -1,9 +1,129 @@
 import type { Config } from "@netlify/edge-functions"
 import { Elysia } from 'elysia'
 import { contents } from './handlers/icon.ts'
-import { article } from "./layouts/article.ts";
 
 import { home } from "./layouts/hn.ts";
+
+import { html, unsafeHTML } from "https://ghuc.cc/worker-tools/html";
+
+// export interface Item {
+//     id: number;
+//     title: string;
+//     points: number | null;
+//     user: string | null;
+//     time: number;
+//     time_ago: string;
+//     content: string;
+//     deleted?: boolean;
+//     dead?: boolean;
+//     type: string;
+//     url?: string;
+//     domain?: string;
+//     comments: Item[]; // Comments are items too
+//     level: number;
+//     comments_count: number;
+//   }
+const comment = (content: any) => html`
+<details open id=${content.id}>
+  <summary>
+    <span><a href="/user/${content.user}">${content.user}</a> - <a href="#${content.id}">${content.time_ago}</a></span
+    >
+  </summary>
+  <div>${unsafeHTML(content.content)}</div>
+  <ul>
+    ${content.comments.map((content: any) => html`<li>${comment(content)}</li>`)}
+  </ul>
+</details>`;
+
+export const article = (content: any) => html`
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="/icon.svg">
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        background-color: whitesmoke;
+        margin: 40px auto;
+        max-width: 80ch;
+        line-height: 1.6;
+        font-size: 18px;
+        color: #444;
+        padding: 0 10px;
+      }
+      details {
+        background-color: whitesmoke;
+        margin: 40px auto;
+        max-width: 650px;
+        line-height: 1.6;
+        font-size: 18px;
+        color: #444;
+      }
+      summary {
+        font-weight: bold;
+        margin: -.5em -.5em 0;
+        padding: .5em;
+      }
+      details[open] summary {
+        border-bottom: 1px solid #aaa;
+      }
+      pre {
+        white-space: pre-wrap;
+      }
+      ul {
+        padding-left: 1em;
+        list-style: none;
+      }
+      h1,h2,h3 {
+        line-height: 1.2;
+        font-size: x-large;
+        margin: 0;
+      }
+      hr {
+        border: 0.5em solid rgba(0,0,0,.1);
+        margin: 2em 0;
+      }
+      article {
+        padding-left: 1em;
+      }
+
+      small {
+        display: block;
+        padding-top: .5em;
+      }
+      p {
+        padding-block: 0.5em;
+        margin: 0;
+      }
+    </style>
+    <title>
+      NFHN: ${content.title}
+    </title>
+  </head>
+  <body>
+    <nav>
+      <a href="/">Home</a>
+    </nav>
+    <hr>
+    <article>
+      <a href="${content.url}">
+        <h1>${content.title}</h1>
+        <small>${content.domain}</small>
+      </a>
+      <p>
+        ${content.points} points by <a href="/user/${content.user}">${content.user}</a> ${content.time_ago}
+      </p>
+    </article>
+    <hr />
+    ${unsafeHTML(content.content)}
+    ${content.comments.map(comment)}
+  </body>
+</html>`;
 
 export const app = new Elysia()
   .onError(({ code, error, set }) => {
