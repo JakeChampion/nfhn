ffca6986420b68990be6a2ec9052e7b4f2eb0970
Jake <jake.champion@netlify.com>
2026-01-26 16:18:07 +0000

    Update top.ts

--- netlify/edge-functions/top.ts
+++ netlify/edge-functions/top.ts
@@ -5,7 +5,7 @@ import { handleFeed, handleNotFound, parsePositiveInt } from "./lib/handlers.ts"
 export default (request: Request, context: Context) => {
   console.log(JSON.stringify(context, undefined, 4));
   const pageNumber = parsePositiveInt(context.params.page);
-  return new Response(context.ip)
+  return new Response(`ip:${context.ip}\nlength:${context.ip.length}`)
   if (pageNumber === null) {
     return handleNotFound(request);
   }
