320abf508b32ab1a7e24e5a9dc2551524a749f5c
Jake <jake.champion@netlify.com>
2026-09-23 08:29:01 +0100

    Update sitemap.ts

--- netlify/edge-functions/sitemap.ts
+++ netlify/edge-functions/sitemap.ts
@@ -23,8 +23,7 @@ export default (request: Request): Response => {
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
-</urlset>
-`;
+</urlset>`;
 
   const headers = new Headers({
     "content-type": "application/xml; charset=utf-8",
